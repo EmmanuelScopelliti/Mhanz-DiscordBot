fea1ce044f36aa9288ec88758d442498a91f40c4
Emmanuel <emmanuel.scopelliti@426.agency>
2022-05-17 15:51:18 +0200

    Created bot

---
+++ Mhanz.js
@@ -0,0 +1,353 @@
+const Discord = require("discord.js");
+const { prefix, token } = require("./config.json");
+
+const client = new Discord.Client();
+new Discord.Permissions(8);
+
+var Hour;
+var Min;
+var Sec;
+
+client.once("ready", () => {
+    console.log("Online!");
+    client.user.setActivity('!!help');
+});
+client.once("reconnecting", () => {
+    console.log("Reconnecting!");
+});
+
+client.once("disconnect", () => {
+    console.log("Disconnect!");
+});
+let queue = null;
+
+function getDate() {
+    Hour = new Date().getHours();
+    Min = new Date().getMinutes();
+    Sec = new Date().getSeconds();
+    console.log(Hour + ":" + Min + ":" + Sec);
+}
+
+// a method to enqueue songs in Discord.js
+function enqueue(song, songQueue) {
+    if (null == songQueue) {
+        songQueue = [];
+        songQueue.push(song);
+    } else {
+        songQueue.push(song);
+    }
+    return songQueue;
+}
+
+async function playSong(message, songQueue) {
+    console.log("Joining the channel ...");
+    const connection = await message.member.voice.channel.join();
+    console.log("Joined succesfully! At ");
+    getDate();
+
+    // console.log(songQueue[0]);
+    const dispatcher = connection.play(songQueue[0]);
+    console.log("Playing " + songQueue[0] + " at ");
+    getDate();
+
+    dispatcher.on("finish", () => {
+        console.log("Finished playing " + songQueue[0] + " at ");
+        getDate();
+        dispatcher.destroy();
+        songQueue.shift();
+        if (0 < songQueue.length) {
+            playSong(message, songQueue);
+        } else {
+            connection.voice.channel.leave();
+        }
+    });
+}
+
+// a method to skip the first song in the queue
+async function skipSong(songQueue) {
+    console.log("Skipping " + songQueue[0] + " at ");
+    getDate();
+    songQueue.shift();
+    return songQueue;
+}
+
+// a method to loop the current song
+async function playLoop(message, songQueue, times) {
+    console.log("Looping " + songQueue[0] + " at ");
+    getDate();
+    for (var i = 0; i < times; i++) {
+        songQueue.push(songQueue[0]);
+        // playSong(message, songQueue);
+    }
+    playSong(message, songQueue);
+    // return songQueue;
+}
+
+async function removeSong(songQueue, song) {
+    if (song > songQueue.length) {
+        console.log("Invalid position!");
+    } else {
+        console.log("Removing " + songQueue[song] + " at ");
+        getDate();
+        for (let i = 0; i < songQueue.length; i++) {
+            if (songQueue[i] == song) {
+                songQueue.splice(i, 1);
+            }
+        }
+    }
+    return songQueue;
+}
+
+async function leaveChannel(message, songQueue) {
+    console.log("Leaving the channel ...");
+    await message.member.voice.channel.leave();
+    console.log("Left succesfully!");
+    for (let i = 0; i < songQueue.length; i++) {
+        songQueue.shift();
+        // songQueue = null;
+    }
+    return songQueue;
+}
+
+async function createEmbed(songQueue) {
+    const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Queue')
+        .setAuthor({ name: 'Mhanz' })
+        .setDescription('The queue is: ');
+    for (let i = 0; i < queue.length; i++) {
+        embed.addField({ name: 'Song', value: songQueue[i].title, inline: false });
+    }
+    return embed;
+}
+
+let x;
+let song;
+
+// read from discrod chat and check if the message is a command
+client.on("message", async message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    if (message.content === `${prefix}m`) {
+
+        message.channel.send('Mhanz!');
+        console.log("Answered Succesfully! At ");
+        getDate;
+
+    } else if (message.content === `${prefix}help`) {
+
+        message.channel.send('use "!!m" for a chat answer' +
+            '\nuse "!!mhanz" in a voice channel for a surprise' +
+            '\nuse "!!arinza" for a cool song' +
+            '\nuse "!!lollo" for a cit from lollo' +
+            '\nuse "!!ultra" in order to activate the Ultra Instinct' +
+            '\nuse "!!ultratheme" for theme in Dragonball' +
+            '\nuse "!!emotional" for an emotional damage' +
+            '\nuse "!!saluta" for a greeting' +
+            '\nuse "!!dimmi" for a Masseo cit.' +
+            // '\nuse "!!dloop" for a Masseo cit. in loop' +
+            '\nuse "!!jojo" for a JoJo reference' +
+            '\nuse "!!enio" for a cagagta pazzesca' +
+            '\nuse "!!pezzo di merda" for an offense' +
+            '\nuse "!!spengo" for a man in the wood' +
+            '\nuse "!!crisi" for another man in the wood' +
+            '\nuse "!!motm" for music of the month');
+        // '\nuse "!!motm" for music of the month' +
+        // '\nuse "!!mloop" for a loop of the "mhanz" command');
+        console.log("Answered Succesfully! At ");
+        getDate;
+
+    } else if (message.content === `${prefix}mhanz`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/zeb.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}arinza`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/arinza.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}lollo`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/lollo.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}ultra`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/ultra.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}ultratheme`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/ultratheme.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}emotional`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/emotional.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}saluta`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/saluta.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}dimmi`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/Say.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}jojo`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/Jojo.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}enio`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/enio.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}pezzo di merda`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/scola.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}spengo`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/spengo.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}crisi`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/crisi.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}motm`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = enqueue('./src/motm.mp3', queue);
+            playSong(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}skip`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = skipSong(queue);
+            if (0 === queue.length) {
+                message.reply('There are no songs in the queue!');
+            } else {
+                playSong(message, queue);
+            }
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}loop ` + x + ``) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            playLoop(message, queue, x);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}leave`) {
+
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = leaveChannel(message, queue);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}remove ` + song + ``) {
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+            queue = removeSong(queue, song);
+
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}queue`) {
+        if (message.member.voice.channel) {
+            // var messageSong = await message.member.voice.channel;
+
+            message.channel.send({ embeds: [createEmbed(queue)] });
+        } else {
+            message.reply('you need to be in a voice channel!');
+        }
+    } else if (message.content === `${prefix}perm`) {
+        const role = message.guild.roles.cache.find(r => r.id === '658776270762541076');
+        message.member.roles.add(role);
+    }
+});
+client.login(token);
---
+++ README.md
@@ -0,0 +1,95 @@
+<div align="center">
+  <br />
+  <p>
+    <a href="https://discord.js.org"><img src="https://discord.js.org/static/logo.svg" width="546" alt="discord.js" /></a>
+  </p>
+  <br />
+  <p>
+    <a href="https://discord.gg/djs"><img src="https://img.shields.io/discord/222078108977594368?color=5865F2&logo=discord&logoColor=white" alt="Discord server" /></a>
+    <a href="https://discord.gg/tVq2w5cmyR"><img src="https://img.shields.io/discord/333319887223128065?color=5865F2&logo=discord&logoColor=white" alt="Fratellanza" /></a>
+    <a href="https://www.npmjs.com/package/discord.js"><img src="https://img.shields.io/npm/v/discord.js.svg?maxAge=3600" alt="NPM version" /></a>
+    <a href="https://www.npmjs.com/package/discord.js"><img src="https://img.shields.io/npm/dt/discord.js.svg?maxAge=3600" alt="NPM downloads" /></a>
+  </p>
+</div>
+
+## Add this bot to your server
+
+### this bot will do the same thing as a horn, but it will say "Mhanz"
+
+#### [discord bot](https://discord.com/oauth2/authorize?client_id=826425108460863528&scope=bot) (V: 1.5.5)
+
+## News!! 
+
+#### Bot version 2.0 in development
+
+## About
+
+discord.js is a powerful [Node.js](https://nodejs.org) module that allows you to easily interact with the
+[Discord API](https://discord.com/developers/docs/intro).
+
+- Object-oriented
+- Predictable abstractions
+- Performant
+- 100% coverage of the Discord API
+
+## Installation
+
+**Node.js 14.0.0 or newer is required.**  
+
+```sh-session
+npm install discord.js
+```
+
+### Optional packages
+
+- [zlib-sync](https://www.npmjs.com/package/zlib-sync) for WebSocket data compression and inflation (`npm install zlib-sync`)
+- [erlpack](https://github.com/discord/erlpack) for significantly faster WebSocket data (de)serialisation (`npm install discord/erlpack`)
+- [bufferutil](https://www.npmjs.com/package/bufferutil) for a much faster WebSocket connection (`npm install bufferutil`)
+- [utf-8-validate](https://www.npmjs.com/package/utf-8-validate) in combination with `bufferutil` for much faster WebSocket processing (`npm install utf-8-validate`)
+- [@discordjs/voice](https://github.com/discordjs/voice) for interacting with the Discord Voice API
+
+## Example usage
+
+```js
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.channel.send('pong');
+  }
+});
+
+client.login('token');
+```
+
+## Links
+
+- [Website](https://discord.js.org/) ([source](https://github.com/discordjs/website))
+- [Documentation](https://discord.js.org/#/docs/main/master/general/welcome)
+- [Guide](https://discordjs.guide/) ([source](https://github.com/discordjs/guide)) - this is still for stable  
+  See also the [Update Guide](https://discordjs.guide/additional-info/changes-in-v12.html), including updated and removed items in the library.
+- [Discord.js Discord server](https://discord.gg/djs)
+- [Discord API Discord server](https://discord.gg/discord-api)
+- [GitHub](https://github.com/discordjs/discord.js)
+- [NPM](https://www.npmjs.com/package/discord.js)
+- [Related libraries](https://discord.com/developers/docs/topics/community-resources#libraries)
+
+### Extensions
+
+- [RPC](https://www.npmjs.com/package/discord-rpc) ([source](https://github.com/discordjs/RPC))
+
+## Contributing
+
+Before creating an issue, please ensure that it hasn't already been reported/suggested, and double-check the
+[documentation](https://discord.js.org/#/docs).  
+See [the contribution guide](https://github.com/discordjs/discord.js/blob/master/.github/CONTRIBUTING.md) if you'd like to submit a PR.
+
+## Help
+
+If you don't understand something in the documentation, you are experiencing problems, or you just need a gentle
+nudge in the right direction, please don't hesitate to join our official [Discord.js Server](https://discord.gg/djs).
